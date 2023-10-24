2959e6076724340a7a8194a2e2f91f31f1938349
David Saez <dsaezslim@gmail.com>
2023-10-24 17:46:47 -0300

    Estructura del footer creada.

--- myboya-project/src/App.jsx
+++ myboya-project/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <h1>Aquí van los componentes, como por ejemplo, arriba el nav, abajo el footer</h1>
+      <Footer/>
     </>
   )
 }
---
+++ myboya-project/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+const Footer = () => {
+  return(
+    <footer>
+      <h2>MyB<span>o</span>ya</h2>
+      <main>
+        <p>Síguenos</p>
+        <section>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </section>
+        <p>Contacto</p>
+        <address>
+          <img src="" alt="Icono de carta para el contacto." />
+          <a href="mailto:myboya@gmail.com">myboya@gmail.com</a>
+        </address>
+        <small>MyBoya™ Santiago de Chile, 2023</small>
+      </main>
+    </footer>
+  )
+}
+
+export default Footer
