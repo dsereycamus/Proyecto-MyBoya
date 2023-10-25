08652f2b4b52207b7b92d4c4f3e3fe2a4614541e
David Saez <dsaezslim@gmail.com>
2023-10-25 18:58:58 -0300

    style(footer): colors and sizes added.

--- myboya-project/src/components/Footer/Footer.jsx
+++ myboya-project/src/components/Footer/Footer.jsx
@@ -1,21 +1,23 @@
+import './Footer.css'
+
 const Footer = () => {
   return(
-    <footer>
-      <h2>MyB<span>o</span>ya</h2>
-      <main>
-        <p>Síguenos</p>
-        <section>
+    <footer className="footer-container">
+      <h2 className='footer__title'>MyB<span className='footer__title__o'>o</span>ya</h2>
+      <section className='footer__info'>
+        <p className='footer__info__title'>Síguenos</p>
+        <div>
           <a href=""></a>
           <a href=""></a>
           <a href=""></a>
-        </section>
-        <p>Contacto</p>
-        <address>
-          <img src="" alt="Icono de carta para el contacto." />
-          <a href="mailto:myboya@gmail.com">myboya@gmail.com</a>
+        </div>
+        <p className='footer__info__title'>Contacto</p>
+        <address className='footer__adress'>
+          <a href="mailto:myboya@gmail.com" className='footer__adress__a'>
+          <img src="" alt="Icono de carta para el contacto." /> myboya@gmail.com</a>
         </address>
-        <small>MyBoya™ Santiago de Chile, 2023</small>
-      </main>
+        <small className='footer__copy'>MyBoya™ Santiago de Chile, 2023</small>
+      </section>
     </footer>
   )
 }
