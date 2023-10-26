8a2b9a730270f81ede486f6e9265dc96c7212fcf
Daniela Serey <dsereycamus@gmail.com>
2023-10-26 10:00:00 -0300

    Agregando formulario contacto

---
+++ myboya-project/src/components/Contacto/Contacto.jsx
@@ -0,0 +1,40 @@
+
+import "./Contacto.css"
+
+const Contacto = () => {
+    return (
+        <div className="container">
+            <div className="form-container">
+                <div className="form-text">
+                    <h1 className="title-contact">
+                        <span className="white-text">¡Mantengamos</span> <span className="orange-text">contacto!</span>
+                    </h1>
+                    <p className="white-text">¿Tienes alguna pregunta o necesitas ayuda?</p>
+                    <p className="white-text">No dudes en comunicarte con nosotros,</p>
+                    <p className="white-text">estaremos encantados de ayudarte.</p>
+                    <p className="orange-text">¡Qué bueno es escuchar de ti!</p>
+                </div>
+
+                <div className="form-content">
+                    <form>
+                        <label htmlFor="name">Nombre:</label>
+                        <input type="text" name="name" id="name" />
+
+                        <label htmlFor="email">Correo electrónico:</label>
+                        <input type="email" name="email" id="email" />
+
+                        <label htmlFor="subject">Asunto:</label>
+                        <input type="text" name="subject" id="subject" />
+
+                        <label>Tu mensaje:</label>
+                        <textarea name="message" id="message" cols="38" rows="5"></textarea>
+
+                        <button className="btn">Enviar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Contacto;
