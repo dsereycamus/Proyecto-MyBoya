373b483e24b25772ad7270d15224d9243332a0c9
Daniela Serey <dsereycamus@gmail.com>
2023-11-01 16:05:57 -0300

    agregando imágenes a contacto

--- myboya-project/src/App.jsx
+++ myboya-project/src/App.jsx
@@ -3,11 +3,14 @@ import './App.css'
 import Inicio from './components/Inicio/Inicio'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
+import Contacto from './components/Contacto/Contacto'
 
 function App() {
   return (
     <div>
-      <Inicio />
+      <Navbar />
+      {/* <Inicio /> */}
+      <Contacto />
       <Footer />
 
     </div>
