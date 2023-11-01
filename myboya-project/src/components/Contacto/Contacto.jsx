4f8c07cf44c870db3e7b31765032d5461cd837ac
Daniela Serey <dsereycamus@gmail.com>
2023-11-01 17:22:02 -0300

    Modificando formulario contacto para que este se conecte a correo electrónico de MyBoya y se limpie una vez enviado el formulario

--- myboya-project/src/components/Contacto/Contacto.jsx
+++ myboya-project/src/components/Contacto/Contacto.jsx
@@ -1,13 +1,37 @@
 import "./Contacto.css";
 import bolsa from "../../assets/Images/bolsa1.png";
-import halo from "../../assets/Images/halo.png"
+import halo from "../../assets/Images/halo.png";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contacto = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_tyeza2x",
+        "template_jg49wao",
+        form.current,
+        "sdBkDbUsu_78TuC2a"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="container">
       <div className="form-container">
         <div className="form-text">
-        <img src={halo} className="halo-image" />
+          <img src={halo} className="halo-image" />
           <h1 className="title-contact">
             <span className="white-text">¡Mantengamos</span>{" "}
             <span className="orange-text">contacto!</span>
@@ -22,21 +46,23 @@ const Contacto = () => {
 
         <div className="form-content">
           <img src={bolsa} className="bag-image" />
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
+
             <label htmlFor="name">Nombre:</label>
-            <input type="text" name="name" id="name" />
+            <input type="text" name="user_name" id="name"/>
 
             <label htmlFor="email">Correo electrónico:</label>
-            <input type="email" name="email" id="email" />
+            <input type="email" name="user_email" id="email" />
 
             <label htmlFor="subject">Asunto:</label>
-            <input type="text" name="subject" id="subject" />
+            <input type="text" name="user_subject" id="subject" />
 
             <label>Tu mensaje:</label>
             <textarea name="message" id="message" cols="38" rows="5"></textarea>
 
-            <button className="btn">Enviar</button>
-          </form>
+            <button className="btn" type="submit">Enviar</button>
+            
+          </form> 
         </div>
       </div>
     </div>
