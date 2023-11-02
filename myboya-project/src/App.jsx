378b92b2c56d3eca215ea8791ec79e53d804010d
TomasDiazV95 <tignacio123@gmail.com>
2023-11-02 15:09:14 -0300

    feat(App): se modifica rutas de navbar

--- myboya-project/src/App.jsx
+++ myboya-project/src/App.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Inicio from "./components/Inicio/Inicio";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Contacto from "./components/Contacto/Contacto";
+import Sesion from "./components/Sesion/Sesion";
+import About from "./components/About/About";
+/* import InicioQuiz from "./components/InicioQuiz/InicioQuiz";  */
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      {/* <Inicio /> */}
-      <Contacto />
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/contacto" element={<Contacto/>} />
+        <Route path="/sesion" element={<Sesion/>} />
+        <Route path="/about" element={<About/>} />
+        {/* <Route path="/playground" element={<InicioQuiz/>} /> */}
+      </Routes>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
