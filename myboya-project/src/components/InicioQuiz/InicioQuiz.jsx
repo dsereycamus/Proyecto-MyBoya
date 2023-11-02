b13f530a050f6f7dc2d0d67f1bdf5d9b869bc2fd
bastian <sg.bastian@outlook.com>
2023-11-02 19:39:19 -0300

    inicioquiz avanzado

--- myboya-project/src/components/InicioQuiz/InicioQuiz.jsx
+++ myboya-project/src/components/InicioQuiz/InicioQuiz.jsx
@@ -2,28 +2,37 @@
 import "./InicioQuiz.css"
 import foto_ranking from "../../assets/Icons/ranking.png"
 import foto_insignia from "../../assets/Icons/insignia.png"
-
-
+import foto_olas from "../../assets/Icons/ola+algas.png"
 
 const InicioQuiz = () => {
-return (
+    return (
+    <>
         <div className="container">
-            <aside className="rank">
-                <button className="ranking">Ranking <img src={foto_ranking} alt="icono del ranking" /></button>
-                <button className="insignia">Consigue tu insignia<img src={foto_insignia} alt="icono de la insignia" /></button>
-            </aside> 
-
-            <section className="InicioQuiz">
-                        <div>
-                            <button className="btnplay">Play</button>
-                        </div>
-                        <div>
-                            <button className="btnwho">¿Cómo Jugar?</button>
-                        </div>
-            </section> 
-
-
+            <div className="rank">
+                <button className="ranking ranking-insignia">Ranking <img src={foto_ranking} alt="icono del ranking" /></button>
+                <button className="insignia ranking-insignia">Consigue tu insignia<img src={foto_insignia} alt="icono de la insignia" /></button>
+            </div> 
+        </div>     
+        <div>
+            <div className="InicioQuiz">
+                <div>
+                    <button className="btnplay">Play</button>
+                </div>
+                <div>
+                    <button className="btnwho">¿Cómo Jugar?</button>
+                </div>
+            </div> 
         </div>
+        <aside className="datocurioso-contenedor">
+            <div className="datocurioso">
+                <h2>Dato Curioso...</h2>
+                <p>El 96% de las reservas de cobalto, el 84% de níquel o el 79% 
+de manganeso se encuentran en los yacimientos submarinos.
+Por consecuencia, la minería está comenzando a invadir suelos marinos, afectando la flora y fauna de nuestros océanos.</p>  
+            </div>
+            <img src={foto_olas} alt="fondo olas"/>
+        </aside>
+    </>
     );
 }
 
