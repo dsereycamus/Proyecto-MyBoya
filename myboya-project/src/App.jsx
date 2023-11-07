f13cb9ba552c399ea2fa61ae877a186f189b7a49
Daniela Serey <dsereycamus@gmail.com>
2023-11-07 09:32:27 -0300

    Se modifica inicio quiz y router para enlazarjuego

--- myboya-project/src/App.jsx
+++ myboya-project/src/App.jsx
@@ -1,27 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Inicio from "./components/Inicio/Inicio";
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
 import Contacto from "./components/Contacto/Contacto";
 import Sesion from "./components/Sesion/Sesion";
 import About from "./components/About/About";
 import InicioQuiz from "./components/InicioQuiz/InicioQuiz";
 import Quiz from "./components/Quiz";
+import GeneralLayout from "./layouts/GeneralLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/sesion" element={<Sesion />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/playground" element={<InicioQuiz />} />
+        <Route path="/" element={<GeneralLayout />}>
+          <Route index element={<Inicio />} />
+          <Route path="contacto" element={<Contacto />} />
+          <Route path="sesion" element={<Sesion />} />
+          <Route path="about" element={<About />} />
+          <Route path="quiz" element={<Quiz />} />
+          <Route path="playground" element={<InicioQuiz />} />
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
---
+++ myboya-project/src/layouts/GeneralLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
+const GeneralLayout = () => {
+  return (
+    <main>
+      <Navbar />
+      <div style={{ minHeight: "50vh" }}>
+        <Outlet />
+      </div>
+      <Footer />
+    </main>
+  );
+};
+
+export default GeneralLayout;
