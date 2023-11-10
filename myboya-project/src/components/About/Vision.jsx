8b6f2836ed22fa057cd696406106f481fe70cc1e
David Saez <dsaezslim@gmail.com>
2023-11-10 09:24:05 -0300

    feat(style): Mison, Vision and About updated.

--- myboya-project/src/components/About/Vision.jsx
+++ myboya-project/src/components/About/Vision.jsx
@@ -1,7 +1,7 @@
 export const Vision = () => {
   return (
     <section className="vision">
-      <h2 className="vision__title">Nuestra vision</h2>
+      <h2 className="vision__title">Nuestra visión</h2>
       <p className="vision__description">
       Apuntamos a resolver el Objetivo de Desarrollo Sostenible 14.1 de la ONU, que busca de aquí a 2025, prevenir y reducir significativamente la contaminación marina de todo tipo, en particular la producida por actividades realizadas en tierra, incluidos los detritos marinos y la polución por nutrientes.
       </p>
