efa3e12f357d9c95329504756329e687c04fbc3c
David Saez <dsaezslim@gmail.com>
2023-11-10 16:02:50 -0300

    feat(About/Team): About updated.

--- myboya-project/src/components/About/Team.jsx
+++ myboya-project/src/components/About/Team.jsx
@@ -1,18 +1,19 @@
 import AboutBubble from "./AboutBubble"
-import bastian from "../../assets/AboutUs/bastian.png";
-import catalina from "../../assets/AboutUs/cata.png";
-import daniela from "../../assets/AboutUs/dani.png";
-import david from "../../assets/AboutUs/david.png";
-import tomas from "../../assets/AboutUs/tomas.png";
+import { team } from "./teamData"
 export const Team = () => {
+  console.log(team[0])
   return(
     <section className="team">
         <h2 className="team__title">Nuestro equipo</h2>
-        <AboutBubble img={daniela} name="Daniela Serey" />
-        <AboutBubble img={bastian} name="Bastian Ulloa" />
-        <AboutBubble img={catalina} name="Catalina Urzua" />
-        <AboutBubble img={tomas} name="Tomas Diaz" />
-        <AboutBubble img={david} name="David Saez" />
+        <div className="team-card__container">
+        {
+          team.map(person => <AboutBubble
+            key={`${person.name} ${person.lastName}`}
+            person={person}
+            />
+            )
+        }
+        </div>
       </section>
   )
 }
