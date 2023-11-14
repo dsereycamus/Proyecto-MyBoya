e36553850303d3b31fd72d4e7de97441e8f6a3b2
TomasDiazV95 <tignacio123@gmail.com>
2023-11-14 09:43:20 -0300

    se agrega login de usuario

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { createUser } from "./user/createUser";
+import { loginUser } from "./user/login";
 
 // Aquí exportamos todos los controladores:
 module.exports = {
-  createUser
+  createUser,
+  loginUser
 }
