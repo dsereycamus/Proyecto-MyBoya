f3e26c0eebb5878923ae8921073d63b255c19067
bastian <sg.bastian@outlook.com>
2023-11-17 14:46:39 -0300

    noticias 3

---
+++ myboya-project/src/components/Noticias/Noticias.jsx
@@ -0,0 +1,18 @@
+import "./Noticias.css";
+
+
+
+const Noticias = () => {
+
+    return (
+        <div>
+            <img src="https://i.pinimg.com/736x/e6/14/1a/e6141a7fab24476945ce6234692314cc.jpg" alt="natgeo imagen" />
+            <img src="https://i.pinimg.com/736x/e6/14/1a/e6141a7fab24476945ce6234692314cc.jpg" alt="natgeo imagen" />
+            <img src="https://i.pinimg.com/736x/e6/14/1a/e6141a7fab24476945ce6234692314cc.jpg" alt="natgeo imagen" />
+            <img src="https://i.pinimg.com/736x/e6/14/1a/e6141a7fab24476945ce6234692314cc.jpg" alt="natgeo imagen" />
+            <img src="https://i.pinimg.com/736x/e6/14/1a/e6141a7fab24476945ce6234692314cc.jpg" alt="natgeo imagen" />
+            <img src="https://i.pinimg.com/736x/e6/14/1a/e6141a7fab24476945ce6234692314cc.jpg" alt="natgeo imagen" />
+        </div>
+);
+}
+export default Noticias;
