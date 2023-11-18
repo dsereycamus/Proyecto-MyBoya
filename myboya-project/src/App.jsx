6c178581b93bf9d062c807eb21c3b64fff090485
bastian <sg.bastian@outlook.com>
2023-11-18 18:54:33 -0300

    seccion noticias

--- myboya-project/src/App.jsx
+++ myboya-project/src/App.jsx
@@ -13,13 +13,14 @@ import New from "./components/New/New";
 import Instrucciones from "./components/Inicio/Instrucciones";
 import GameLayout from "./layouts/GameLayout";
 import PartnersSection from "./components/Partners/Partners";
+import Noticias from "./components/Noticias/Noticias";
 
 function App() {
   return (
     <BrowserRouter>
       <ParticlesBackground />
       <Routes>
-        <Route path="*" element={<div>No hay página</div>} />
+      <Route path="*" element={<div>No hay página</div>} />
         <Route path="/" element={<GeneralLayout />}>
           <Route index element={<Inicio />} />
           <Route path="contacto" element={<Contacto />} />
@@ -30,6 +31,7 @@ function App() {
           <Route path="playground" element={<InicioQuiz />} />
           <Route path="instrucciones" element={<Instrucciones />} />
           <Route path="partners" element={<PartnersSection />} />
+          <Route path="noticias" element={<Noticias />} />
         </Route>
 
         <Route path="/quiz" element={<GameLayout />}>
