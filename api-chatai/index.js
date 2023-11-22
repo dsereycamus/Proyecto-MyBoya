9c7614a65a7364185581ea918556c0fea6f838d1
David Saez <dsaezslim@gmail.com>
2023-11-22 19:28:12 -0300

    feat(chat) Now you can recieve messages.

--- api-chatai/index.js
+++ api-chatai/index.js
@@ -1,6 +1,34 @@
 import express from 'express'
+import logger from 'morgan'
 import 'dotenv/config'
+
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const port = process.env.PORTCHAT ?? 8074
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server, {cors: {
+  origin: "*"
+}})
+
+io.on('connection', (socket) => {
+  console.log('a user has connected')
+  socket.on('disconnected', () => {
+    console.log('an user has disconnected')
+  })
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg)
+  })
+})
+
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.send('Hola de nuevo')
+})
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
