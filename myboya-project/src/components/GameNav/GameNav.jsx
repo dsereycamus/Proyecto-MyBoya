988e2347b6f9444e798a3c9f0dde361115f7a4af
TomasDiazV95 <tignacio123@gmail.com>
2023-11-23 00:28:58 -0300

    se modifica navbar del juego

--- myboya-project/src/components/GameNav/GameNav.jsx
+++ myboya-project/src/components/GameNav/GameNav.jsx
@@ -1,32 +1,51 @@
 import React from 'react'
 import logo from '../../assets/Images/myboya-logo.png'
-import './GameNav.css'
-import { useRef } from "react";
-import { NavLink, Link } from "react-router-dom";
+import './GameNav.css';
+import { useNavigate, NavLink, Link } from "react-router-dom";
 
 export default function GameNav() {
 
-    const navRef = useRef();
-    const show = () => {
-        navRef.current.classList.toggle("show")
-    }
+    const navigate = useNavigate();
+    const { isLoggedIn, logout: logoutContext } = useSession();
+  
+    const logout = () => {
+      logoutContext();
+      navigate("/");
+    };
+  
 
     return (
         <header>
             <nav className='header-navgame'>
                 <h2 className='title-navgame'>MyB<span className='letter-o'>o</span>ya</h2>
+                <Link to="/">
                 <img src={logo} alt="Logo MyBoya" />
-                <ul className="listaNav2">
+                </Link>
+                {/* <ul className="listaNav2">
                     <li>
                         <NavLink to="/">Inicio</NavLink>
                     </li>
                     <li>
                         <NavLink to="/about">Nosotros</NavLink>
                     </li>
-                </ul>
-                <button className='btn-gamenav' onClick={show}>Cuenta</button>
+                </ul> */}
+                <button className='btn-gamenav2'>Cuenta</button>
+                {isLoggedIn ? (
+                    <div className="btn-registro-nav">
+                        <button onClick={logout}>Cerrar sesión</button>
+                    </div>
+                ) : (
+                    <>
+                        <Link to="/sesion" className="btn-sesion-nav">
+                            <button>Inicio sesión</button>
+                        </Link>
+                        <Link to="/registro" className="btn-registro-nav">
+                            <button>¡Regístrate!</button>
+                        </Link>
+                    </>
+                )}
             </nav>
-            
+
         </header>
     )
 }
