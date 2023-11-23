910c69f6b69b113a3e9ec6ce3e6d511bdfc60ad6
Catalina <catalinabelenurzuadiaz@gmail.com>
2023-11-22 22:33:34 -0300

    insignias bloqueadas y desbloqueadas -cuenta

--- myboya-project/src/components/Cuenta/Cuenta.jsx
+++ myboya-project/src/components/Cuenta/Cuenta.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import "./cuenta.css";
 import estrella from "../../assets/Images/estrella.png";
-import puntaje from "../../assets/Images/puntaje.png";
+import puntajeImg from "../../assets/Images/puntaje.png";
 import insignia1 from "../../assets/Images/insignia1.png";
 import insignia2 from "../../assets/Images/insignia2.png";
 import insignia3 from "../../assets/Images/insignia3.png";
@@ -13,39 +13,17 @@ function Cuenta() {
   const { userData } = useSession();
   const navigate = useNavigate();
   const jugarQuiz = () => navigate("/playground");
+  const puntaje = userData?.score ?? 0;
 
   /* Personalizar color ícono */
   const [color, setColor] = useState("#80BACC");
 
-  const cambiarColor1 = () => {
-    const nuevoColor = "#80BACC";
-    setColor(nuevoColor);
-  };
-
-  const cambiarColor2 = () => {
-    const nuevoColor = "#78C36C";
-    setColor(nuevoColor);
-  };
-
-  const cambiarColor3 = () => {
-    const nuevoColor = "#CA80CC";
-    setColor(nuevoColor);
-  };
-
-  const cambiarColor4 = () => {
-    const nuevoColor = "#B23737";
-    setColor(nuevoColor);
-  };
-
-  const cambiarColor5 = () => {
-    const nuevoColor = "#FF8C00";
-    setColor(nuevoColor);
-  };
-
-  const cambiarColor6 = () => {
-    const nuevoColor = "#E3CE12";
-    setColor(nuevoColor);
-  };
+  const cambiarColor1 = () => setColor("#80BACC");
+  const cambiarColor2 = () => setColor("#78C36C");
+  const cambiarColor3 = () => setColor("#CA80CC");
+  const cambiarColor4 = () => setColor("#B23737");
+  const cambiarColor5 = () => setColor("#FF8C00");
+  const cambiarColor6 = () => setColor("#E3CE12");
   /* fin Personalizar color ícono */
 
   return (
@@ -67,11 +45,11 @@ function Cuenta() {
           </div>
           <h2 className="nombreUsuario">{userData?.name ?? ""}</h2>
           <p className="emailUsuario">{userData?.email ?? ""}</p>
-          <p className="opcionesUsuario">Modificar mis datos</p>
+          {/*           <p className="opcionesUsuario">Modificar mis datos</p> */}
         </div>
         <div className="containerPuntos">
           <div className="puntajePerfil">
-            <img src={puntaje} className="img-puntaje"></img>
+            <img src={puntajeImg} className="img-puntaje"></img>
             <div className="contenedorpuntajeAcumulado">
               <h2 className="puntajeAcumulado">PUNTAJE TOTAL ACUMULADO</h2>
               <p className="cantidadPuntajeAcumulado">
@@ -81,33 +59,94 @@ function Cuenta() {
           </div>
           <h2 className="misInsignias">MIS INSIGNIAS</h2>
           <div className="insigniasPerfil">
-            <div className="contenedor-insignia">
-              <img src={insignia1} className="img-insignia"></img>
-              <p className="tituloInsignia">Aprendiz</p>
-              <p className="puntosFaltantesInsignia">Consigue 50 puntos</p>
-            </div>
-            <div className="contenedor-insignia">
-              <img src={insignia2} className="img-insignia"></img>
-              <p className="tituloInsignia">Principiante</p>
-              <p className="puntosFaltantesInsignia">Consigue 300 puntos</p>
-            </div>
-            <div className="contenedor-insignia">
-              <img src={insignia3} className="img-insignia"></img>
-              <p className="tituloInsignia">Intermedio</p>
-              <p className="puntosFaltantesInsignia">Consigue 1000 puntos</p>
-            </div>
-            <div className="contenedor-insignia">
-              <img src={insignia4} className="img-insignia"></img>
-              <p className="tituloInsignia">Avanzado</p>
-              <p className="puntosFaltantesInsignia">Consigue 1500 puntos</p>
-            </div>
+            {puntaje >= 1500 && (
+              <div className="contenedor-insignia-padre">
+                <div className="contenedor-insignia">
+                  <img src={insignia1} className="img-insignia"></img>
+                  <p className="tituloInsignia">Aprendiz</p>
+                  <p className="puntosFaltantesInsignia">
+                    Conseguiste 50 puntos
+                  </p>
+                </div>
+                <div className="contenedor-insignia">
+                  <img src={insignia2} className="img-insignia"></img>
+                  <p className="tituloInsignia">Principiante</p>
+                  <p className="puntosFaltantesInsignia">
+                    Conseguiste 300 puntos
+                  </p>
+                </div>
+                <div className="contenedor-insignia">
+                  <img src={insignia3} className="img-insignia"></img>
+                  <p className="tituloInsignia">Intermedio</p>
+                  <p className="puntosFaltantesInsignia">
+                    Conseguiste 1000 puntos
+                  </p>
+                </div>
+                <div className="contenedor-insignia">
+                  <img src={insignia4} className="img-insignia"></img>
+                  <p className="tituloInsignia">Avanzado</p>
+                  <p className="puntosFaltantesInsignia">
+                    Conseguiste 1500 puntos
+                  </p>
+                </div>
+              </div>
+            )}
+            {puntaje >= 1000 && puntaje < 1500 && (
+              <div className="contenedor-insignia-padre">
+                <div className="contenedor-insignia">
+                  <img src={insignia1} className="img-insignia"></img>
+                  <p className="tituloInsignia">Aprendiz</p>
+                  <p className="puntosFaltantesInsignia">
+                    Conseguiste 50 puntos
+                  </p>
+                </div>
+                <div className="contenedor-insignia">
+                  <img src={insignia2} className="img-insignia"></img>
+                  <p className="tituloInsignia">Principiante</p>
+                  <p className="puntosFaltantesInsignia">
+                    Conseguiste 300 puntos
+                  </p>
+                </div>
+                <div className="contenedor-insignia">
+                  <img src={insignia3} className="img-insignia"></img>
+                  <p className="tituloInsignia">Intermedio</p>
+                  <p className="puntosFaltantesInsignia">
+                    Consigue 1000 puntos
+                  </p>
+                </div>
+              </div>
+            )}
+            {puntaje >= 300 && puntaje < 1000 && (
+              <div className="contenedor-insignia-padre">
+                <div className="contenedor-insignia">
+                  <img src={insignia1} className="img-insignia"></img>
+                  <p className="tituloInsignia">Aprendiz</p>
+                  <p className="puntosFaltantesInsignia">
+                    Conseguiste 50 puntos
+                  </p>
+                </div>
+                <div className="contenedor-insignia">
+                  <img src={insignia2} className="img-insignia"></img>
+                  <p className="tituloInsignia">Principiante</p>
+                  <p className="puntosFaltantesInsignia">
+                    Conseguiste 300 puntos
+                  </p>
+                </div>
+              </div>
+            )}
+            {puntaje >= 50 && puntaje < 300 && (
+              <div className="contenedor-insignia">
+                <img src={insignia1} className="img-insignia"></img>
+                <p className="tituloInsignia">Aprendiz</p>
+                <p className="puntosFaltantesInsignia">Conseguiste 50 puntos</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
-      <div className="botones-cuenta">
-        <button className="btn-cuenta">Guardar cambios</button>
-        <button onClick={jugarQuiz} className="btn-cuenta">Volver a página principal</button>
-      </div>
+      <button onClick={jugarQuiz} className="btn-cuenta">
+        Volver a página principal
+      </button>
     </div>
   );
 }
